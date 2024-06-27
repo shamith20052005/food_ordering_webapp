c22694eee4d57c5a1137055dc7d7214d34524135
iamshamith <kshamith2005@gmail.com>
2024-06-27 16:42:35 +0530

    corrected database,for deploying

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Orders, Address
+from .models import Orders, Address,OrderItem
 
 # Register your models here.
 admin.site.register(Orders)
 admin.site.register(Address)
+admin.site.register(OrderItem)
